45e4c171927e05251cb19cb7cc004422ba03a762
Aaliyahnaomi <120403928+Aaliyahnaomi@users.noreply.github.com>
2023-05-30 13:01:33 +0100

    Add files via upload

---
+++ To-Do-List Project/script.js
@@ -0,0 +1,43 @@
+// Select DOM elements
+const form = document.getElementById('todo-form');
+const input = document.getElementById('todo-input');
+const todoList = document.getElementById('todo-list');
+
+// Function to add a new to-do item
+function addTodoItem(event) {
+  event.preventDefault();
+  const task = input.value;
+
+  if (task) {
+    const li = document.createElement('li');
+
+    const checkbox = document.createElement('input');
+    checkbox.type = 'checkbox';
+    checkbox.addEventListener('change', function () {
+      if (this.checked) {
+        li.classList.add('completed');
+      } else {
+        li.classList.remove('completed');
+      }
+    });
+
+    const taskText = document.createElement('span');
+    taskText.innerText = task;
+
+    const deleteBtn = document.createElement('button');
+    deleteBtn.innerText = 'Delete';
+    deleteBtn.addEventListener('click', function () {
+      li.remove();
+    });
+
+    li.appendChild(checkbox);
+    li.appendChild(taskText);
+    li.appendChild(deleteBtn);
+
+    todoList.appendChild(li);
+    input.value = '';
+  }
+}
+
+// Event listener for form submission
+form.addEventListener('submit', addTodoItem);
